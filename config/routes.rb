b183c7a7c62908d824abdaac552151d552fb4852
Maarten <maartendebaecke@gmail.com>
2019-05-15 18:48:57 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :toys
   devise_for :users
   resources :foods
+  get 'toys', to: 'pages#toys'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
